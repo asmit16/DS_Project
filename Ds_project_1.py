228d0e929eaa3f319b6fa71a729cdc7ef4416142
Asmit Pawar <62091376+asmit16@users.noreply.github.com>
2022-11-04 15:03:34 +0530

    Update Ds_project_1.py

--- Ds_project_1.py
+++ Ds_project_1.py
@@ -23,7 +23,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://wallpapercave.com/download/bull-and-bear-wallpapers-wp8399987");
+             background-image: url("https://wallpapercave.com/download/stock-market-wallpapers-wp2128235");
              background-attachment: fixed;
 	     background-position: 25% 75%;
              background-size: cover
